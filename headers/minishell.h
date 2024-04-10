85f6b54f349dbd539b93abd423578bf1c804fd45
Anouk Boulore-Vergez <aboulore@1C5.42angouleme.fr>
2024-04-10 15:53:56 +0200

    .h updates

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aboulore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:11:16 by aboulore          #+#    #+#             */
-/*   Updated: 2024/04/10 14:11:56 by aboulore         ###   ########.fr       */
+/*   Updated: 2024/04/10 14:54:48 by aboulore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 #define MINISHELL_H
 
 # include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <dirent.h>
+# include <termios.h>
 
 #endif
---
+++ nottosubmit/pseudo.md
@@ -0,0 +1,7 @@
+main
+	initialization
+		env parsing that must happen when launching a shell
+		readline
+	while pas exit ou signal genre sigkill
+		routine (input->parsing->exec->EXIT)
+	free
