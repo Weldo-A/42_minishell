49c8850bb4e88c49ed4923b37c4524b005da8240
aboulore <aboulore@gmail.com>
2024-04-13 00:04:23 +0200

    modifs sur parsing.c, mineur

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: aboulore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:33:37 by aboulore          #+#    #+#             */
-/*   Updated: 2024/04/12 17:26:31 by aboulore         ###   ########.fr       */
+/*   Updated: 2024/04/13 00:00:44 by aboulore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ size_t	parsing(char *str, t_list **inputs)
 	size_t		input_nb;
 	char		**inputs_arr;
 
-
 //lexer
 	inputs_arr = newlines(str, &input_nb);
 	print_2d_array(inputs_arr);
 	ft_putstr("\n\n");
 	*inputs = break_into_words(inputs_arr, input_nb); 
 	print_unidentified_tokens(input_nb, *inputs);
-//	identifcation;
 
+//	identification;
 
-//parsr
+//	parser
+
 	return (input_nb);
 }
